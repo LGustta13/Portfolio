ee248e46a6f33d5a7d7258b1e72c39ae3290b029
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-09-13 23:35:23 -0300

    initializating project and creating global.ts styles

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #000000;
+        --red: #FF0000;
+        --shape: #FFFFFF;
+        --white-symbol: #FFFCFC;
+        --gray: #D9D9D9;
+        --title: #282828;
+        --subtitle: #333232;
+        --text: #282828;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media screen and (max-width: 1080px){
+            font-size: 95.00%;
+        }
+
+        @media screen and (max-width: 720px){
+            font-size: 85.5%;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 500;
+    }
+
+    button {
+        cursor: pointer;
+    }
+`
